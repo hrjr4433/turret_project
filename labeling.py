7da83af59858f5752d4cc93753b3dbe96768a7b2
hrjr4433 <hrjr4433@gmail.com>
2015-03-07 22:44:50 -0700

    labeling fixed

--- labeling.py
+++ labeling.py
@@ -4,29 +4,53 @@
 import numpy as np
 from parakeet import jit
 
-
 @jit
-def labeling(bin_img):
+def labeling_first(bin_img):
+    # first pass
     r,c = bin_img.shape
     int_img = np.zeros((r,c),dtype=np.uint8)
-    label = 1
+    label = 0
     for i in xrange(r):
         for j in xrange(c):
             if bin_img[i,j]:
-                if bin_img[i-1,j]:
-                    int_img[i,j] = int_img[i-1,j]
-                elif bin_img[i,j-1]:
-                    int_img[i,j] = int_img[i,j-1]
+                min_r = max(0,i-1)
+                max_r = max(r,i+1)
+                min_c = max(0,j-1)
+                max_c = min(c,j+2)
+                min_label = 0
+                for ii in xrange(min_r,max_r):
+                    for jj in xrange(min_c,max_c):
+                        if bin_img[ii,jj] and int_img[ii,jj]:
+                            if not min_label or (min_label and int_img[ii,jj] < min_label):
+                                min_label = int_img[ii,jj]
+                if min_label:
+                    int_img[i,j] = min_label
                 else:
                     int_img[i,j] = label
                     label += 1
+    for i in xrange(r):
+        for j in xrange(c):
+            if bin_img[i,j]:
+                max_r = max(r,i+5)
+                max_c = min(c,j+5)
+                min_label = 0
+                for ii in xrange(i,max_r):
+                    for jj in xrange(j,max_c):
+                        if int_img[ii,jj]:
+                            if not min_label or (min_label and int_img[ii,jj] < min_label):
+                                min_label = int_img[ii,jj]
+                int_img[i,j] = min_label
     return int_img
 
-def label_items(bin_img, min=30):
-    int_img = labeling(bin_img)
-    num_items = np.max(int_img)
-    for i in range(1,num_items+1):
+def find_biggest_object(bin_img):
+    int_img = labeling_first(bin_img)
+    num_obj = np.max(int_img)
+    biggest = np.zeros(bin_img.shape, dtype=np.bool)
+    biggest_num_pix = 0
+    for i in range(1,num_obj+1):
         mask = (int_img == i)
-        if mask.sum() < min:
-            int_img[mask] = 0
-    return int_img
+        mask_sum = mask.sum()
+        if  mask_sum > biggest_num_pix:
+            biggest_num_pix = mask_sum
+            biggest = np.array(mask)
+    return biggest
